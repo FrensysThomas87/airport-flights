072d66624e7223d5e6c267f000e3768bd5bc399a
Francesco <delta873@gmail.com>
2021-05-06 19:03:05 +0200

    Tolta variabile  da AirportController

--- app/Http/Controllers/AirportController.php
+++ app/Http/Controllers/AirportController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 
-use App\Airport;
+
 
 class AirportController extends Controller
 {
-    public function index(Airport $cheaperFlight){
-        return view('compare-flights', compact('cheaperFlight'));
+    public function index(){
+        return view('compare-flights');
     }
 
 }
